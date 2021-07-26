4be7f28f5c8985a632ce17360c2bb41aa21043a3
Jean-Simondon <jean@lajungle.fr>
2021-07-26 23:36:09 +0200

    maj

--- command.php
+++ command.php
@@ -1,8 +1,29 @@
 <?php
 
+namespace Init;
+
+use Menu\Menu;
+use Menu\MenuDAO;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+
 WP_CLI::add_command("run_forest", "run_forest");
 
 function run_forest() {
-    echo realpath(".");
-    shell_exec("cd " . realpath(".") . " && php index");
+    require_once __DIR__ . '/config/requirement.php';
+    require_once __DIR__ . '/environnement/Env.php';
+    // require __DIR__ . '/config/set_environnement.php';
+    require __DIR__ . '/config/constant.php';
+    require __DIR__ . '/config/private_constant.php';
+    require_once __DIR__ . '/config/loader.php';
+    
+    ( new Menu( ( new MenuDAO() )->getConfigMenu(), new CliMenuBuilder()) )->getMenu()->open();
+    
+    echo "Bye bye\n";
 }
+
+
+
+
+
+
+
