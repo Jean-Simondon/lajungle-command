7b46cfc19fbcd453c11b024a2734c0d024c77971
Jean-Simondon <jean@lajungle.fr>
2021-07-26 23:31:45 +0200

    maj

--- command.php
+++ command.php
@@ -4,5 +4,5 @@ WP_CLI::add_command("run_forest", "run_forest");
 
 function run_forest() {
     echo "Hello World";
-    // require_once realpath(".") . '/index-for-wp-cli.php';
+    shell_exec("php index");
 }
