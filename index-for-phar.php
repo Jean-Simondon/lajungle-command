8942ff92dae19257cc1b11ba8b92d1ee025650ee
Jean-Simondon <jean@lajungle.fr>
2021-07-26 23:08:30 +0200

    maj

--- index-for-phar.php
+++ index-for-phar.php
@@ -1,3 +1,20 @@
 <?php declare(strict_types=1);
 
+namespace Init;
 
+// define( 'ROOT_PATH', realpath('.'));
+
+// var_dump(ROOT_PATH);
+// die();
+
+require_once __DIR__ . '/config/requirement.php';
+require_once __DIR__ . '/config/constant.php';
+require_once __DIR__ . '/config/private_constant.php';
+require_once __DIR__ . '/config/loader.php';
+
+use Menu\Menu;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+
+( new Menu( $config_menu, new CliMenuBuilder()) )->getMenu()->open();
+
+echo "Bye bye\n";
