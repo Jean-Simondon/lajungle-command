3c1add74dc386f044ebe1e2f3f3839112a8fd154
Jean-Simondon <jean@lajungle.fr>
2021-07-27 22:06:18 +0200

    maj

--- script/scripts_vendor/configuration_ljd_cli/create_config_ini.php
+++ script/scripts_vendor/configuration_ljd_cli/create_config_ini.php
@@ -11,60 +11,60 @@ use WP_CLI;
  * Créer un fichier de config et faire itérer dessus
  * pour toutes les constantes à créer
  */
-$project_root = str_replace( " " , "\ " , PROJECT_ROOT);
+// $project_root = str_replace( " " , "\ " , PROJECT_ROOT);
 
 WP_CLI::line("test dans le fichier de scripts 2\n");
 
-$this->display("Bienvenue dans l'outils de création du fichier de configuration config.ini")
-    ->display("Ce fichier est important afin de guider l'outils ljd-cli dans l'arborescence de votre projet")
+// $this->display("Bienvenue dans l'outils de création du fichier de configuration config.ini")
+//     ->display("Ce fichier est important afin de guider l'outils ljd-cli dans l'arborescence de votre projet");
 
-    ->askInputNumber(
-        "Type de project ?",
-        [
-            '1' => 'iquitheme',
-            '2' => 'loreto'
-        ],
-        "PROJECT_TYPE"
-    )
+//     ->askInputNumber(
+//         "Type de project ?",
+//         [
+//             '1' => 'iquitheme',
+//             '2' => 'loreto'
+//         ],
+//         "PROJECT_TYPE"
+//     )
 
-    ->display("Nom du theme (sans - ni _ ni majuscule")->askInputText("THEME_NAME")
+//     ->display("Nom du theme (sans - ni _ ni majuscule")->askInputText("THEME_NAME")
 
-    ->askInputNumber(
-        "Version de PHP à utiliser ?",
-        [
-            '1' => '5.4',
-            '2' => '5.6',
-            '3' => '7.0',
-            '4' => '7.1',
-            '5' => '7.2',
-            '6' => '7.3',
-            '7' => '7.4'
-        ],
-        "PHP_V"
-    )
+//     ->askInputNumber(
+//         "Version de PHP à utiliser ?",
+//         [
+//             '1' => '5.4',
+//             '2' => '5.6',
+//             '3' => '7.0',
+//             '4' => '7.1',
+//             '5' => '7.2',
+//             '6' => '7.3',
+//             '7' => '7.4'
+//         ],
+//         "PHP_V"
+//     )
 
-    ->display("Merci, c'est terminé");
+//     ->display("Merci, c'est terminé");
 
-$content = "";
+// $content = "";
 
-if ( !empty( $this->get("PROJECT_TYPE") ) ) {
-    $content .= "PROJECT_TYPE=" . $this->get("PROJECT_TYPE");
-    $content = trim($content);
-}
+// if ( !empty( $this->get("PROJECT_TYPE") ) ) {
+//     $content .= "PROJECT_TYPE=" . $this->get("PROJECT_TYPE");
+//     $content = trim($content);
+// }
 
-if (!empty($this->get("THEME_NAME"))) {
-    $content .= "\nTHEME_NAME=" . $this->get("THEME_NAME");
-    $content = trim($content);
-}
+// if (!empty($this->get("THEME_NAME"))) {
+//     $content .= "\nTHEME_NAME=" . $this->get("THEME_NAME");
+//     $content = trim($content);
+// }
 
-if (!empty($this->get("PHP_V"))) {
-    $content .= "\nPHP_V=" . $this->get("PHP_V");
-    $content = trim($content);
-}
+// if (!empty($this->get("PHP_V"))) {
+//     $content .= "\nPHP_V=" . $this->get("PHP_V");
+//     $content = trim($content);
+// }
 
-if (!empty($content)) {
-    $this->display("Création du fichier config-for-ljd-cli.ini")
-        ->exec("cd " . $project_root . " && echo \"" . $content . "\" > config-for-ljd-cli.ini")
-        ->display("C'est un succès, vous pouvez retrouver le fichier config-for-ljd-cli.ini à la racine du projet")
-        ->display("IMPORTANT : vous devez quitter et relancer ljd.phar pour prendre ce fichier en compte");
-}
+// if (!empty($content)) {
+//     $this->display("Création du fichier config-for-ljd-cli.ini")
+//         ->exec("cd " . $project_root . " && echo \"" . $content . "\" > config-for-ljd-cli.ini")
+//         ->display("C'est un succès, vous pouvez retrouver le fichier config-for-ljd-cli.ini à la racine du projet")
+//         ->display("IMPORTANT : vous devez quitter et relancer ljd.phar pour prendre ce fichier en compte");
+// }
